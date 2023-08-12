b267af09362dc9077c6f38427ae67339c7eeac0a
q13x <84165981+WorldEditAxe@users.noreply.github.com>
2023-08-12 01:55:48 -0700

    Much more detailed CommandContext

--- workerconcurrency/src/main/java/me/q13x/workerconcurrency/ipc/CommandContext.java
+++ workerconcurrency/src/main/java/me/q13x/workerconcurrency/ipc/CommandContext.java
@@ -1,14 +1,43 @@
 package me.q13x.workerconcurrency.ipc;
 
+import me.q13x.workerconcurrency.RemoteWorkerManager;
+import me.q13x.workerconcurrency.RemoteWorkerSlave;
+import me.q13x.workerconcurrency.WorkerManager;
+import me.q13x.workerconcurrency.WorkerSlave;
+import me.q13x.workerconcurrency.errors.BadArgumentException;
 import me.q13x.workerconcurrency.wrappers.IPCAdapter;
 
 public class CommandContext {
     EnvironmentType environmentType;
     IPCAdapter socket;
 
-    public CommandContext(EnvironmentType envType, IPCAdapter ipcSocket) {
+    RemoteWorkerSlave remoteSlave = null;
+    RemoteWorkerManager remoteManager = null;
+
+    WorkerSlave slave;
+    WorkerManager manager;
+
+
+    public CommandContext(EnvironmentType envType, IPCAdapter ipcSocket, Object remoteObject, Object currentObject) {
         this.environmentType = envType;
         this.socket = ipcSocket;
+
+        if (!(remoteObject instanceof RemoteWorkerSlave) && !(remoteObject instanceof RemoteWorkerManager)) {
+            throw new BadArgumentException("You must pass a RemoteWorkerSlave or a RemoteWorkerManager as the remote object.");
+        } else if (!(currentObject instanceof WorkerSlave) && !(currentObject instanceof WorkerManager)) {
+            throw new BadArgumentException("You must pass a WorkerSlave or a WorkerManager as the current object.");
+        } else {
+            if (remoteObject instanceof RemoteWorkerSlave) {
+                this.remoteSlave = (RemoteWorkerSlave) remoteObject;
+            } else {
+                this.remoteManager = (RemoteWorkerManager) remoteObject;
+            }
+            if (currentObject instanceof WorkerSlave) {
+                this.slave = (WorkerSlave) currentObject;
+            } else {
+                this.manager = (WorkerManager) currentObject;
+            }
+        }
     }
 
     public EnvironmentType getEnvironmentType() {
@@ -19,8 +48,40 @@ public class CommandContext {
         return this.socket;
     }
 
+    public RemoteWorkerManager getRemoteManager() {
+        if (this.environmentType == EnvironmentType.MANAGER) {
+            throw new IllegalStateException("Requested remote master in a master context!?");
+        } else {
+            return remoteManager;
+        }
+    }
+
+    public RemoteWorkerSlave getRemoteSlave() {
+        if (this.environmentType == EnvironmentType.SLAVE) {
+            throw new IllegalStateException("Requested remote slave in a slave context!?");
+        } else {
+            return remoteSlave;
+        }
+    }
+
+    public WorkerManager getWorkerManager() {
+        if (this.environmentType == EnvironmentType.SLAVE) {
+            throw new IllegalStateException("Requested WorkerManager in a slave context!?");
+        } else {
+            return manager;
+        }
+    }
+
+    public WorkerSlave getWorkerSlave() {
+        if (this.environmentType == EnvironmentType.MANAGER) {
+            throw new IllegalStateException("Requested WorkerSlave in a master context!?");
+        } else {
+            return slave;
+        }
+    }
+
     public enum EnvironmentType {
-        MASTER(0),
+        MANAGER(0),
         SLAVE(1);
 
         int networkValue;
