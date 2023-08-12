9c6123795b932551ab8da9123df6f75c8dc98e03
q13x <84165981+WorldEditAxe@users.noreply.github.com>
2023-08-12 01:57:08 -0700

    Private enum fields, added CommandEnum#getCommandClassInstance

--- workerconcurrency/src/main/java/me/q13x/workerconcurrency/CommandEnum.java
+++ workerconcurrency/src/main/java/me/q13x/workerconcurrency/CommandEnum.java
@@ -1,5 +1,6 @@
 package me.q13x.workerconcurrency;
 
+import me.q13x.workerconcurrency.ipc.commands.init.SMReadyCommand;
 import org.teavm.jso.browser.Window;
 
 import java.util.function.BiConsumer;
@@ -13,13 +14,16 @@ import me.q13x.workerconcurrency.ipc.commands.SMPongCommand;
  * This Java class contains a list of valid cross-worker IPC commands.
  */
 public enum CommandEnum {
+    SM_READY((short) -1, false, SMReadyCommand.class, (packet, context) -> {
+        // TODO: implement code
+    }),
     MS_PING((short) 0, true, MSPingCommand.class, (packet, context) -> {
         if (context.getEnvironmentType() == CommandContext.EnvironmentType.SLAVE) {
             context.getIPCAdapter().write(new SMPongCommand(((MSPingCommand) packet).getRequestId()).toBuffer());
         }
     }),
     SM_PONG((short) 1, false, SMPongCommand.class, (packet, context) -> {
-        if (context.getEnvironmentType() == CommandContext.EnvironmentType.MASTER) {
+        if (context.getEnvironmentType() == CommandContext.EnvironmentType.MANAGER) {
             Window.alert("Received pong from worker!");
             Window.setTimeout(() -> {
                 context.getIPCAdapter().write(new MSPingCommand(((SMPongCommand) packet).getRequestId()).toBuffer());
@@ -27,10 +31,10 @@ public enum CommandEnum {
         }
     });
 
-    short id;
-    boolean boundToSlave;
-    Class<? extends ICommand> commandClass;
-    BiConsumer<ICommand, CommandContext> commandCallback;
+    private short id;
+    private boolean boundToSlave;
+    private Class<? extends ICommand> commandClass;
+    private BiConsumer<ICommand, CommandContext> commandCallback;
 
     CommandEnum(short id, boolean boundToSlave, Class<? extends ICommand> commandClass, BiConsumer<ICommand, CommandContext> commandCallback) {
         this.id = id;
@@ -45,6 +49,10 @@ public enum CommandEnum {
 
     public boolean isBoundToSlave() {
         return this.boundToSlave;
+    }
+
+    public ICommand getCommandClassInstance() throws InstantiationException, IllegalAccessException {
+        return this.commandClass.newInstance();
     }
 
     public Class<? extends ICommand> getCommandClass() {
